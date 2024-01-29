84ae72784dc4c2d67d9e490cf8a3d7d4f9d4200b
B07Y3N <botyen_razvan@yahoo.com>
2024-01-29 18:52:45 +0200

    modul2

---
+++ modul2/part1.py
@@ -0,0 +1,20 @@
+str1 = 'hello world'
+print(type(str1))
+int1 = 100
+print(type(int1))
+none = None
+print(type(none))
+
+new = result = str1.capitalize()
+print(type(result))
+
+print('Returned type:', type(result))
+
+str1 = 'hello world {} {}'
+result = str1.format('test', 'test2')
+print('Formatted String:', result)
+
+
+str1 = 'hello world'
+result = str1.center(30, '#')
+print('Center String:', result)
