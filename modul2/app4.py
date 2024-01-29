d13ce50712b623bc4132c9dbd5a63696c26422c8
B07Y3N <botyen_razvan@yahoo.com>
2024-01-29 19:56:58 +0200

    modul2

---
+++ modul2/app4.py
@@ -0,0 +1,13 @@
+num1 = int(input('nr1: '))
+num2 = int(input('nr2: '))
+
+average = (num1 + num2) / 2
+print(average)
+
+# Integer division
+int_division = num1 // num2
+print("Integer division =", int_division)
+
+# num1 ** num2
+power = num1 ** num2
+print("num1 ** num2 =", power)
