c7449d4baaf873c67463d3899fe9c01ae6b2da93
B07Y3N <botyen_razvan@yahoo.com>
2024-01-31 21:05:58 +0200

    modul2

---
+++ modul2/app6.py
@@ -0,0 +1,30 @@
+print('Astazi ma duc la "facultate."')
+
+# r - n-o sa interpreteze string-ul
+
+print(r'/*\/*\/*\/*\ ')
+
+print('Python\n', r'\./\./.\./', sep="")
+
+#t = Tab
+print("P\ty\tt\th\to\tn")
+
+print('Astazi ma duc la "facultate".')
+print("Astazi ma duc la \"facultate\".")
+
+print(r'/*\/*\*/*\/*\ ', '\nPython\n', r'\./\./\./\./ ', sep='')
+
+print('P','y','t','h','o','n',sep='    ')
+
+
+print('Astazi ma duc la "facultate".')
+
+print(r"""/*\/*\*/*\/*\
+Python
+\./\./\./\./""")
+
+print("P", "y", "t", "h", "o", "n", sep="   ")
+
+
+
+
---
+++ modul2/app8.py
@@ -0,0 +1,6 @@
+sir = input("Introduceti un sir: ")
+
+print("Lungimea sirului este: {} ".format(len(sir)))
+
+sir = input('Introduceti un sir: ')
+print(f'Lungimea sirului este: {len(sir)}')
